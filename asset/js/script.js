518f1ed672e8fb88f09a4803daf89234b96fd34d
Ayano2304 <Marthawij23@gmail.com>
2023-05-30 20:33:38 +0700

    sampe footer

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,17 @@
-const menu = document.querySelector('.menu')
-const toggle = document.querySelector('.mobile_menu')
+const menu = document.querySelector('.menu') // Tombolnya 
+const toggle = document.querySelector('.mobile_menu') //Menu List nya
 
 menu.addEventListener('click', () => {
     toggle.classList.toggle('hidden')
 })
+
+document.querySelectorAll('.gambar img').forEach(image => {
+    image.onclick = () => {
+        document.querySelector('.popup_img').style.display = 'block'
+        document.querySelector('.popup_img img').src = image.getAttribute('src')
+    }
+})
+
+document.querySelector(".popup_img span").onclick = () =>{
+    document.querySelector(".popup_img").style.display = "none";
+}
